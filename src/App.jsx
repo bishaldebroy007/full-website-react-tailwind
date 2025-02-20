8baf1c769342bc6e3fc652097dc688216c503c95
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-20 17:38:48 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
